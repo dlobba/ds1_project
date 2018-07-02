a458c030da7dc5639cf17b066279488efc0b9856
dlobba <diego.lobba@studenti.unitn.it>
2018-07-02 18:09:07 +0200

    Crash cases written. Still to see them in action thought

--- src/main/java/reliable_multicast/Participant.java
+++ src/main/java/reliable_multicast/Participant.java
@@ -7,30 +7,40 @@ import reliable_multicast.messages.*;
 
 public class Participant extends BaseParticipant {
 	
-	public enum CrashType {
+	public enum MutlicastCrashType {
 		MULTICAST_N_CRASH,
 		MULTICAST_ONE_N_CRASH,
+	}
+	public enum ReceivingCrashType {
 		RECEIVE_VIEW_N_CRASH,
 		RECEIVE_MULTICAST_N_CRASH
 	}
 	
-	public class SendCrashMsg implements Serializable {
-		final CrashType type;
-		public SendCrashMsg(CrashType type) {
+	public class SendMulticastCrashMsg implements Serializable {
+		final MutlicastCrashType type;
+		public SendMulticastCrashMsg(MutlicastCrashType type) {
+			this.type = type;
+		}
+	};
+	public class SendReceivingCrashMsg implements Serializable {
+		final ReceivingCrashType type;
+		public SendReceivingCrashMsg(ReceivingCrashType type) {
 			this.type = type;
 		}
 	};
 	
 	protected ActorRef groupManager;
 	protected boolean crashed;
-	protected boolean doAndCrash;
+	protected boolean receiveMessageAndCrash;
+	protected boolean receiveViewChangeAndCrash;
 	
 	// Constructors
 	public Participant(ActorRef groupManager) {
 		super();
 		this.groupManager = groupManager;
 		this.crashed = false;
-		this.doAndCrash = false;
+		this.receiveMessageAndCrash = false;
+		this.receiveViewChangeAndCrash = false;
 		this.groupManager.tell(new JoinRequestMsg(), this.getSelf());
 	}
 	
@@ -70,7 +80,33 @@ public class Participant extends BaseParticipant {
 	protected void onViewChangeMsg(ViewChangeMsg viewChange) {
 		if (this.crashed)
 			return;
-		super.onViewChangeMsg(viewChange);
+		
+		if (this.receiveViewChangeAndCrash)
+			this.crashAfterViewChange(viewChange);
+		else
+			super.onViewChangeMsg(viewChange);
+	}
+	
+	/**
+	 * Replicate the behavior of the normal view-change method,
+	 * but instead of sending the FLUSH messages the node crashes.
+	 * @param viewChange
+	 */
+	protected void crashAfterViewChange(ViewChangeMsg viewChange) {
+		System.out.printf("%d P-%d P-%d INFO started_view-change V%d\n",
+				System.currentTimeMillis(),
+				this.id,
+				this.id,
+				viewChange.view.id);
+		this.flushesReceived.clear();
+		this.tempView = new View(viewChange.view);
+		for (Message message : messagesUnstable) {
+			for (ActorRef member : this.tempView.members) {
+				member.tell(message, this.getSelf());
+			}
+		}
+		this.getSelf().tell(new CrashMsg(), this.getSelf());
+		// FLUSHES are not sent
 	}
 
 	@Override
@@ -85,6 +121,14 @@ public class Participant extends BaseParticipant {
 		if (this.crashed)
 			return;
 		super.onReceiveMessage(message);
+
+		if (this.receiveMessageAndCrash) {
+			// remove the flag (so when the node revives
+			// it won't crash suddenly).
+			// Then let the node crash.
+			this.receiveMessageAndCrash = false;
+			this.getSelf().tell(new CrashMsg(), this.getSelf());
+		}
 	}
 	
 	// EXT: external behavior message handlers --
@@ -113,85 +157,90 @@ public class Participant extends BaseParticipant {
 	// EXT --------------------------------------
 
 	// implementing sending and receiving -------
-		// variants with crashes --------------------
+	// variants with crashes --------------------
 		
-		private void multicastAndCrash() {
-			if (! this.canSend)
-				return;
-			
-			// this node cannot send message
-			// until this one is completed
-			this.canSend = false;
+	private void multicastAndCrash() {
+		if (! this.canSend)
+			return;
+		
+		// this node cannot send message
+		// until this one is completed
+		this.canSend = false;
 
-			Message message = new Message(this.id,
-					this.multicastId,
-					false);
-			this.multicastId += 1;
-			
-			for (ActorRef member : this.view.members) {
-				member.tell(message, this.getSelf());
-			}
-			// Do not send stable messages.
-			// Crash instead
-			this.getSelf().tell(new CrashMsg(), this.getSelf());
-		}
+		Message message = new Message(this.id,
+				this.multicastId,
+				false);
+		this.multicastId += 1;
 		
-		/**
-		 * While performing a multicast, the message
-		 * is effectively sent just to one other actor, then
-		 * the sender crashes.
-		 */
-		private void multicastOneAndCrash() {
-			if (! this.canSend)
-				return;
-			// this node cannot send messages
-			// until this one is completed
-			this.canSend = false;
+		for (ActorRef member : this.view.members) {
+			member.tell(message, this.getSelf());
+		}
+		// Do not send stable messages.
+		// Crash instead
+		this.getSelf().tell(new CrashMsg(), this.getSelf());
+	}
+	
+	/**
+	 * While performing a multicast, the message
+	 * is effectively sent just to one other actor, then
+	 * the sender crashes.
+	 */
+	private void multicastOneAndCrash() {
+		if (! this.canSend)
+			return;
+		// this node cannot send messages
+		// until this one is completed
+		this.canSend = false;
 
-			ActorRef[] members = this.view.members.toArray(new ActorRef[0]);
-			if (members.length < 2) {
-				System.out.printf("%d P-%d P-%s WARNING: too few view members."
-						+ "Crash denied. Multicast aborted. \n",
-						System.currentTimeMillis(),
-						this.id,
-						this.id);
-				return;
-			}
-			
-			Message message = new Message(this.id,
-					this.multicastId,
-					false);
-			this.multicastId += 1;
-			ActorRef receiver = members[0];
-			
-			// manage the unlucky case in which
-			// the only node seeing the message is
-			// the crashing node...
-			if (receiver.equals(this.getSelf()))
-				receiver = members[1];
-			
-			receiver.tell(message, this.getSelf());
-			
-			// let the sender crash
-			this.getSelf().tell(new CrashMsg(), this.getSelf());
+		ActorRef[] members = this.view.members.toArray(new ActorRef[0]);
+		if (members.length < 2) {
+			System.out.printf("%d P-%d P-%s WARNING: too few view members."
+					+ "Crash denied. Multicast aborted. \n",
+					System.currentTimeMillis(),
+					this.id,
+					this.id);
+			return;
 		}
 		
-		protected void onSendCrashMsg(SendCrashMsg crashMsg) {
-			switch (crashMsg.type) {
-			case MULTICAST_N_CRASH:
-				this.multicastAndCrash();
-				break;
-			case MULTICAST_ONE_N_CRASH:
-				this.multicastOneAndCrash();
-				break;
-			case RECEIVE_MULTICAST_N_CRASH:
-				this.doAndCrash = true; 
-				break;
-			case RECEIVE_VIEW_N_CRASH:
-				this.doAndCrash = true;
-				break;
-			}
+		Message message = new Message(this.id,
+				this.multicastId,
+				false);
+		this.multicastId += 1;
+		ActorRef receiver = members[0];
+		
+		// manage the unlucky case in which
+		// the only node seeing the message is
+		// the crashing node...
+		if (receiver.equals(this.getSelf()))
+			receiver = members[1];
+		
+		receiver.tell(message, this.getSelf());
+		
+		// let the sender crash
+		this.getSelf().tell(new CrashMsg(), this.getSelf());
+	}
+	
+	protected void onSendMutlicastCrashMsg(SendMulticastCrashMsg crashMsg) {
+		switch (crashMsg.type) {
+		case MULTICAST_N_CRASH:
+			this.multicastAndCrash();
+			break;
+		case MULTICAST_ONE_N_CRASH:
+			this.multicastOneAndCrash();
+			break;
 		}
+	}
+	
+	protected void onReceivingMulticastCrashMsg(SendReceivingCrashMsg crashMsg) {
+		switch (crashMsg.type) {
+		case RECEIVE_MULTICAST_N_CRASH:
+			this.receiveMessageAndCrash = true;
+			break;
+		case RECEIVE_VIEW_N_CRASH:
+			this.receiveViewChangeAndCrash = true;
+			break;
+		}
+	}
 	
 	@Override
 	public Receive createReceive() {
@@ -204,7 +253,10 @@ public class Participant extends BaseParticipant {
 				.match(SendMulticastMsg.class, this::onSendMulticastMsg)
 				.match(CrashMsg.class, this::onCrashMsg)
 				.match(ReviveMsg.class, this::onReviveMsg)
-				.match(SendCrashMsg.class, this::onSendCrashMsg)
+				.match(SendMulticastCrashMsg.class,
+						this::onSendMutlicastCrashMsg)
+				.match(SendReceivingCrashMsg.class,
+						this::onReceivingMulticastCrashMsg)
 				//.match(AliveMsg.class, this::onAliveMsg)
 				.build();
 	}
