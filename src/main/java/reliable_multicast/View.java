a06ac35d8c65d1df950fad4f5d3cb482c45e42b0
dlobba <diego.lobba@studenti.unitn.it>
2018-07-04 11:41:30 +0200

    Added equals to View class

--- src/main/java/reliable_multicast/View.java
+++ src/main/java/reliable_multicast/View.java
@@ -1,5 +1,6 @@
 package reliable_multicast;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -30,6 +31,27 @@ public class View {
 
 	public Set<ActorRef> getMembers() {
 		return new HashSet<>(members);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (obj instanceof View) {
+			View other = (View) obj;
+			if (id != other.id)
+				return false;
+			if (members == null) {
+				if (other.members != null)
+					return false;
+			}
+			if (!members.containsAll(other.members)
+				|| !other.members.containsAll(members))
+				return false;
+		}
+		return true;
 	}
 
 	@Override
